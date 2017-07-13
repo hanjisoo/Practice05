4313c84e039f7b2f0bc0edaee08dcb496eb4e6ce
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-13 18:56:54 +0900

    다더하기 아직안함

--- src/com/javaex/problem03/Goods.java
+++ src/com/javaex/problem03/Goods.java
@@ -6,6 +6,34 @@ public class Goods {
     private int price;
     private int count;
     
-    
+    public Goods() {
+    	
+    }
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = Integer.parseInt(price);
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(String count) {
+		this.count = Integer.parseInt(count);
+	}
+	public void showInfo() {
+		System.out.println(name+"(가격:"+price+"원)이"+count+"개 입고 되었습니다.");
+	}
     
 }
--- src/com/javaex/problem03/GoodsApp.java
+++ src/com/javaex/problem03/GoodsApp.java
@@ -6,9 +6,28 @@ import java.util.Scanner;
 
 public class GoodsApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-       
-    }
+		List<Goods> goods = new ArrayList<Goods>();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("상품 3개를 입력해주세요");
+		
+		for(int i=0; i<3; i++) {
+			String line=sc.nextLine();
+			String[] data=line.split(" ");
+			
+			Goods good=new Goods();
+			good.setName(data[0]);
+			good.setPrice(data[1]);
+			good.setCount(data[2]);
+			goods.add(good);
+		}
+		
+
+		for(int i=0; i<goods.size(); i++) {
+			goods.get(i).showInfo();
+		}
+		sc.close();
+	}
 
 }
